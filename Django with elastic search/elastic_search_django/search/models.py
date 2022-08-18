a34ba5927a89871f1d5754e3c87de787a2aa4481
SiddharthAsodariya <siddharthasodariya2701@gmail.com>
2022-08-18 11:39:17 +0530

    v1.5

--- Django with elastic search/elastic_search_django/search/models.py
+++ Django with elastic search/elastic_search_django/search/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class ElasticDemo(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField(max_length=255 )
+    content = models.TextField(max_length=255)
 
     def __str__(self):
         return self.title
